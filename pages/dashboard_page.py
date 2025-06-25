05d9550c3919d0e02e0426ac1215f0daeafa1106
zimaev <zimaew@gmail.com>
2025-06-25 09:59:27 +0300

    Знакомство с PageObject. Практика работы с Page Object

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -8,6 +8,6 @@ class DashboardPage(BasePage):
         super().__init__(page)
         self.dashboard_toolbar_title = page.get_by_test_id("dashboard-toolbar-title-text")
 
-    def check_dashboard_title(self):
+    def check_visible_dashboard_title(self):
         expect(self.dashboard_toolbar_title).to_be_visible()
 
