75887945800ef0335caca42bfcac1866f918bab8
zimaev <zimaew@gmail.com>
2025-06-30 19:53:42 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -12,21 +12,16 @@ class SidebarListItemComponent(BaseComponent):
     def __init__(self, page: Page, prefix: str):
         super().__init__(page)
 
-        self.icon = Icon(page,f'{prefix}-drawer-list-item-icon',"Icon")
-        self.title = Text(page,f'{prefix}-drawer-list-item-title-text', "Title")
-        self.button = Button(page,f'{prefix}-drawer-list-item-button', "Button")
+        self.icon = Icon(page, f'{prefix}-drawer-list-item-icon', "Icon")
+        self.title = Text(page, f'{prefix}-drawer-list-item-title-text', "Title")
+        self.button = Button(page, f'{prefix}-drawer-list-item-button', "Button")
 
     def check_visible(self, title: str):
-        # expect(self.icon).to_be_visible()
         self.icon.check_visible()
 
-        # expect(self.title).to_be_visible()
         self.title.check_visible()
-        # self.title.check_have_text(title)
-        # expect(self.title).to_have_text(title)
         self.title.check_have_text(title)
 
-        # expect(self.button).to_be_visible()
         self.button.check_visible()
 
     def navigate(self, expected_url: Pattern[str]):
